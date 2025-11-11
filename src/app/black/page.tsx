43b5b58b48f8ad481da2199eb8eea3afd7b339df
aprimoresisedu <contato@aprimoresisedu.com>
2025-11-11 14:17:19 -0300

    Add files via upload

---
+++ src/app/black/page.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { Form2 } from "@/components/form2";
+import Header from "@/components/header";
+import Image from "next/image";
+import { useState } from "react";
+
+interface PageProps {
+  searchParams: {
+    utm_content?: string
+    utm_campaign?: string
+    utm_term?: string
+    utm_source?: string
+    utm_medium?: string
+  }
+}
+
+export default function Home({ searchParams }: PageProps) {
+
+  const [modal, setModal] = useState<boolean>(false)
+
+  return (
+    <div className="bg-zinc-950">
+      <Form2
+        modal={modal}
+        setModal={setModal}
+      />
+      </div>
+      <Header
+        btnLabel="RESGATAR INGRESSO GRÁTIS"
+        imagePath="bg3"
+        title={<>Seminário Exclusivo: Torne-se almejada no Mercado de Trabalho para ser contratada ou conquistar um 2° vínculo como enfermeira</>}
+        subtitle={<>Para resgatar GRÁTIS um dos ingressos ainda disponíveis, é só apertar no botão abaixo enquanto ainda dá tempo:</>}
+        modal={modal}
+        setModal={setModal}
+        searchParams={searchParams}
+      />
+      <footer className="px-4 text-center -mt-2 bg-black py-4 flex justify-center text-white">
+        <p>Dona do Plantão. Todos os direitos reservados. Políticas de Privacidade.</p>
+      </footer>
+    </div>
+  );
+}
